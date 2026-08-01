663000c138b054602935df187e8a3b40edefe8f8
rakeshk6110 <rakesh.kesavan30@email.com>
2026-08-01 17:35:16 +0530

    frontend deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
 // localStorage.setItem("access","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzg0NTQxMjA2LCJpYXQiOjE3ODQ1NDA5MDYsImp0aSI6IjllMDdjMTlkMDI1NzQ0NmY5ODBmZmM5ZjU2YzM1ZjJhIiwidXNlcl9pZCI6IjMifQ.E1La3WDusWlSFFh5gdV3t-NZcCZDRwhbPiJG9e0RYF4")
 const API = axios.create({
-  baseURL: "https://ai-crop-disease-detection-and-famer.onrender.com/api",
+  baseURL: `{API_URL}`,
 });
 
 API.interceptors.request.use((config) => {
