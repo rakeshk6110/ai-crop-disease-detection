bf7513426722e965ddd2b1bd7a65b34eef0c6d36
rakeshk6110 <rakesh.kesavan30@email.com>
2026-08-01 17:24:48 +0530

    testing api

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -128,8 +128,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173/",
+    "https://YOUR-FRONTEND.onrender.com",
+]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
