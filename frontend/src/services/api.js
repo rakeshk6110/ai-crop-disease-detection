8e4a505f8d0fd9c652df83473a8465064d850b86
rakeshk110 <rakeshrh6767@gmail.com>
2026-08-03 21:55:21 +0530

    ...

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,6 +7,7 @@ const API = axios.create({
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("access");
+  console.log(API)
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
